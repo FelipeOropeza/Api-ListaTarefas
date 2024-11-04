1d9857448a0723d872f2654f14791de8b0aa0b7e
FelipeOropeza <felipe2006.co@gmail.com>
2024-11-04 18:30:18 -0300

    feat: mudança no sql

--- sql/task_api.sql
+++ sql/task_api.sql
@@ -1,26 +1,25 @@
 create database task_api;
 use task_api;
 
-CREATE TABLE users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+create table users (
+    id int primary key auto_increment,
+    name varchar(50) not null unique,
+    email varchar(100) not null unique,
+    password varchar(255) not null,
+    created_at timestamp default current_timestamp
 );
 
-CREATE TABLE tasks (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    description TEXT,
-    status ENUM('pendente', 'em_progresso', 'concluida') DEFAULT 'pendente',
-    priority ENUM('baixa', 'media', 'alta') DEFAULT 'media',
-    due_date DATE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id)
-        REFERENCES users (id)
-        ON DELETE CASCADE
+create table tasks (
+    id int primary key auto_increment,
+    user_id int not null,
+    title varchar(255) not null,
+    description text,
+    status enum('pendente', 'em_progresso', 'concluida') default 'pendente',
+    priority enum('baixa', 'media', 'alta') default 'media',
+    due_date date,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp on update current_timestamp,
+    foreign key (user_id)
+        references users (id)
+        on delete cascade
 );
