50734d667845321544f79f863ef414213b8f2ba8
FelipeOropeza <felipe2006.co@gmail.com>
2024-12-29 09:31:44 -0300

    feat: adição do cors pra controlar onde a API pode ser utilizada

--- public/index.php
+++ public/index.php
@@ -2,8 +2,9 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use Felipe\ApiListatarefa\Helper\TokenHelper;
+use App\Helper\TokenHelper;
 use Slim\Factory\AppFactory;
+use Tuupola\Middleware\CorsMiddleware;
 
 $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
@@ -12,6 +13,16 @@ TokenHelper::initializeKey();
 
 $app = AppFactory::create();
 
-(require __DIR__ . '../../app/Routes/Routes.php')($app);
+$app->add(new CorsMiddleware([
+    "origin" => ["*"],
+    "methods" => ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    "headers.allow" => ["Authorization", "Content-Type", "Accept", "Origin"],
+    "headers.expose" => ["Authorization"],
+    "credentials" => true,
+    "cache" => 0,
+]));
+
+(require __DIR__ . '/../app/Routes/Routes.php')($app);
+
 
 $app->run();
